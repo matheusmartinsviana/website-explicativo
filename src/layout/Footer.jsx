8fe1671850b05c8a983dcaeb6235e8e016585801
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-08-06 20:21:43 -0300

    feat: adding footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,40 @@
-function Footer(){
+import styles from './Styles/Footer.module.css';
+
+function Footer() {
     return (
-        <footer>
-        footer
+        <footer className={styles.footerContainer}>
+            <section className={styles.footerSection}>
+                <a href="#">Descomplicando Linguagens e Ferramentas de Desenvolvimento</a>
+                <p>Este site reflete o estudo nos tópicos de ferramentas e linguagens apresentadas neste website.</p>
+            </section>
+            <section className={styles.footerSection}>
+                <h2>Mapa do site</h2>
+                <ul>
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">JS</a></li>
+                    <li><a href="#">React</a></li>
+                    <li><a href="#">Node.js</a></li>
+                    <li><a href="#">DevOps</a></li>
+                </ul>
+            </section>
+            <section className={styles.footerSection}>
+                <h3>Colaboradores</h3>
+                <ul>
+                    <li>
+                        <a href="#">Matheus Martins Viana</a>
+                        <i></i>
+                    </li>
+                    <li>
+                        <a href="#">Dayane Nogueira</a>
+                        <i></i>
+                    </li>
+                    <li>
+                        <a href="#">Davi Finkler</a>
+                        <i></i>
+                    </li>
+                </ul>
+            </section>
         </footer>
     );
 }
