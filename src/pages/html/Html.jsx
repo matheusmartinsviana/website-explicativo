a7e5102826c0497ae4ffbd742bc78d2af32663fc
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-11-02 17:19:02 -0300

    feat: adicionando estrutura padrão para html e js

--- src/pages/html/Html.jsx
+++ src/pages/html/Html.jsx
@@ -1,8 +1,44 @@
+import "../styles/html.css";
 
 export default function Html() {
-    return (
-        <>
-            Html
-        </>
-    )
+  return (
+    <>
+      <div className="html-structures-container">
+        <h2>Estruturas HTML</h2>
+        <ul>
+          <li>
+            <strong>Estruturais:</strong> <code>&lt;html&gt;</code>,{" "}
+            <code>&lt;head&gt;</code>, <code>&lt;body&gt;</code>,{" "}
+            <code>&lt;div&gt;</code>, <code>&lt;span&gt;</code>
+            <p>nav</p>
+          </li>
+          <li>
+            <strong>Textuais:</strong> <code>&lt;h1&gt;</code> -{" "}
+            <code>&lt;h6&gt;</code>, <code>&lt;p&gt;</code>,{" "}
+            <code>&lt;a&gt;</code>, <code>&lt;strong&gt;</code>,{" "}
+            <code>&lt;em&gt;</code>
+          </li>
+          <li>
+            <strong>Mídia:</strong> <code>&lt;img&gt;</code>,{" "}
+            <code>&lt;audio&gt;</code>, <code>&lt;video&gt;</code>
+          </li>
+          <li>
+            <strong>Formulários:</strong> <code>&lt;form&gt;</code>,{" "}
+            <code>&lt;input&gt;</code>, <code>&lt;textarea&gt;</code>,{" "}
+            <code>&lt;button&gt;</code>, <code>&lt;select&gt;</code>
+          </li>
+          <li>
+            <strong>Tabelas:</strong> <code>&lt;table&gt;</code>,{" "}
+            <code>&lt;tr&gt;</code>, <code>&lt;td&gt;</code>,{" "}
+            <code>&lt;th&gt;</code>
+          </li>
+          <li>
+            <strong>Semânticas:</strong> <code>&lt;header&gt;</code>,{" "}
+            <code>&lt;footer&gt;</code>, <code>&lt;article&gt;</code>,{" "}
+            <code>&lt;section&gt;</code>, <code>&lt;nav&gt;</code>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/pages/js/Javascript.jsx
+++ src/pages/js/Javascript.jsx
@@ -1,8 +1,30 @@
+import "../styles/js.css";
 
 export default function Javascript() {
-    return (
-        <>
-            javascript
-        </>
-    )
+  return (
+    <>
+      {/* Liste as funções e explique as principais utilizações do JavaScript no
+      frontend. Inclua exemplos práticos. Manipulação de DOM:
+      document.getElementById, document.querySelector, document.createElement,
+      element.appendChild Eventos: addEventListener, eventos comuns (click,
+      submit, mouseover, keydown) Manipulação de Dados: Arrays (map, filter,
+      reduce), Objetos (criação e acesso), Funções (declaração, expressão, arrow
+      functions) Fetch API: fetch, Promises, Async/Await */}
+
+      <div className="js-elements">
+        <ul className="js-elements-list">
+          <li className="js-element">
+            <h3>getElementById</h3>
+            <p>Basta declarar uma variável e utilizar seguinte função:</p>
+            <code>const text = documento.getElementById(&quot;text&quot;)</code>
+          </li>
+          <li>
+            <h3>querySelector</h3>
+            <p></p>
+            <code></code>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
