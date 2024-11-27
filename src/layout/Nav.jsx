ec04223fd4dfa4bede8ba3c7f03e0e26d9df41a8
Matheus Martins Viana <matheusmviana@outlook.com.br>
2024-11-26 22:43:17 -0300

    fix: css import

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -4,7 +4,6 @@ import GlobalTheme from "../globals";
 import { lightTheme, darkTheme } from "../theme";
 import Switch from "../components/Switch.jsx";
 import { Link, useNavigate } from "react-router-dom";
-import "./Styles/nav.css";
 
 function Nav() {
   const [isToggled, setIsToggled] = useState(false);
